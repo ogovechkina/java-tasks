9f57f9ce0f4e22918dbbc21e261b717dd7d5eb54
Ovechkina <o.ovechkina2012@yandex.ru>
2023-03-12 13:23:46 +0300

    Массивы: задача 1

--- arrays-tasks/src/main/java/org/example/Main.java
+++ arrays-tasks/src/main/java/org/example/Main.java
@@ -2,6 +2,19 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        test1();
+    }
+
+
+    public static void test1 () {
+        int[] num = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        int maxNum = num.length;
+
+        for (int i = 0; i < maxNum; i++) {
+            if (num[i] < 6) num[i] = num[i] * 2;
+
+            System.out.print(i+1 + "-" + num[i] + " ");
+        }
+
     }
 }
