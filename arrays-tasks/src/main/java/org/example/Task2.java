2738cd9ae5b0d791cfcae9ee52684bc8c724a2ae
Ovechkina <o.ovechkina2012@yandex.ru>
2023-03-22 10:32:24 +0300

    Массивы: задача 2

---
+++ arrays-tasks/src/main/java/org/example/Task2.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class Task2 {
+
+    public static void main(String[] args) {
+        int[][] square = new int[5][5];
+        for (int i = 0; i < square.length; i++) {
+            square[i][i] = 1;
+            square[i][square.length - i - 1] = 1;
+        }
+        for (int i = 0; i < square.length; i++) {
+            for (int j = 0; j < square.length; j++) {
+                System.out.print(square[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
