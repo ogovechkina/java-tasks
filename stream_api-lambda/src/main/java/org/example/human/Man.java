dfaa192b973932fba36afb25009f15fcd1779628
Ovechkina <o.ovechkina2012@yandex.ru>
2023-04-04 19:41:00 +0300

    Приавки по streamApi

--- stream_api-lambda/src/main/java/org/example/human/Man.java
+++ stream_api-lambda/src/main/java/org/example/human/Man.java
@@ -1,7 +1,5 @@
 package org.example.human;
 
-import org.example.human.Human;
-
 public class Man extends Human {
 
     public Man(String name, int age) {
--- stream_api-lambda/src/main/java/org/example/human/Woman.java
+++ stream_api-lambda/src/main/java/org/example/human/Woman.java
@@ -1,7 +1,5 @@
 package org.example.human;
 
-import org.example.human.Human;
-
 public class Woman extends Human {
 
     public Woman(String name, int age) {
